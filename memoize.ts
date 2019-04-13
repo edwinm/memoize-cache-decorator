48227e1cba6f1ab6f87cf9d2292ece7fb72730cd
Edwin Martin <edwin@bitstorm.org>
2019-04-13 21:52:35 +0200

    Format code

--- memoize.ts
+++ memoize.ts
@@ -1,23 +1,23 @@
 export function memoize() {
-    return function(
-        target: Object,
-        propertyName: string,
-        propertyDesciptor: PropertyDescriptor
-    ): PropertyDescriptor {
-        const fn = propertyDesciptor.value;
-		    const map = new Map();
+	return function(
+		target: Object,
+		propertyName: string,
+		propertyDesciptor: PropertyDescriptor
+	): PropertyDescriptor {
+		const fn = propertyDesciptor.value;
+		const map = new Map();
 
-        propertyDesciptor.value = function(...args) {
-          let json = JSON.stringify(args);
-            if (map.has(json)) {
-                return map.get(json);
-            } else {
-                const result = fn.apply(this, args);
-                map.set(json, result);
-                return result;
-            }
-        };
+		propertyDesciptor.value = function(...args) {
+			let json = JSON.stringify(args);
+			if (map.has(json)) {
+				return map.get(json);
+			} else {
+				const result = fn.apply(this, args);
+				map.set(json, result);
+				return result;
+			}
+		};
 
-        return propertyDesciptor;
-    };
+		return propertyDesciptor;
+	};
 }
